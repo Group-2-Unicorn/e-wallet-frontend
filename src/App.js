98684cd2725068225f1a90a7c7a00dc67bc33c01
Isah Jacob <isahjacob0@gmail.com>
2023-01-24 17:38:59 +0100

    added a button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Verification from './Components/Verification';
+import Verification from '../src/Components/VerifyPage/Verification'
 
 function App() {
   return (
     <div>
+      <button>Verify</button>
       <Verification />
     </div>
   );

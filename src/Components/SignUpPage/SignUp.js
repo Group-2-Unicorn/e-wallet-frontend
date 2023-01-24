34ae94768bef1f7ac6480c83e22c057e095a6b4d
Isah Jacob <isahjacob0@gmail.com>
2023-01-24 20:47:50 +0100

    added an image to the sign up page

---
+++ src/Components/SignUpPage/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import images from '../Assets/img.jpeg'
+
+
+function SignUp(){
+    return (
+        <div>
+            <div className="left">
+                <img src={images} alt="logo" />
+            </div>
+        </div>
+    )
+}

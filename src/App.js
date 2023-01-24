045ee5aa46dc7c06908b9513e3c86c7e619a6261
Isah Jacob <isahjacob0@gmail.com>
2023-01-24 21:16:17 +0100

    Added Sign up component to app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Verification from '../src/Components/VerifyPage/Verification'
+import SignUp from './Components/SignUpPage/SignUp';
 
 function App() {
   return (
     <div>
       <button>Verify</button>
+      <SignUp />
       <Verification />
     </div>
   );

a9aa1b76b63c93d77df7e179f417fda7e3ae1805
Isah Jacob <isahjacob0@gmail.com>
2023-01-24 17:38:03 +0100

    added html structure for the verification page

--- src/Components/VerifyPage/Verification.js
+++ src/Components/VerifyPage/Verification.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import images from '../Assets/img.jpeg'
 function Verification() {
   return (
-    <div>
-      <h1>Verification</h1>
+    <div className="Container">
+      <div className="content">
+        <div className="header">
+          <h1 className="title"> Verification</h1>
+        </div>
+        
+        <div>
+          <button>Close</button>
+        </div>
+      </div>
     </div>
   );
 }

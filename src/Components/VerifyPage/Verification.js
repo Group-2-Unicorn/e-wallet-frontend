aaa7d6017349c9d269284a8646b89d69d0625a85
Isah Jacob <isahjacob0@gmail.com>
2023-01-25 21:06:56 +0100

    added images and changed the verification page

--- src/Components/VerifyPage/Verification.js
+++ src/Components/VerifyPage/Verification.js
@@ -3,7 +3,10 @@ import './verify.css'
 
 function Verification() {
   return (
-    <div className="Container">
+    <div className="very-container">
+      <div className="left-side">   
+      <img className="images" src={image} alt=""/>
+      </div>
       <div className="content-container">
         <div className="content-holder">
           <h1 className="content"> Kindly Check your mail to see your OTP number for Account verification</h1>

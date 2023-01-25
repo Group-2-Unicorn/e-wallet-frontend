b9cb523d47a8b94c9a237840a8062c618d573dc4
Samuel Timothy <lordtimothy11@gmail.com>
2023-01-25 09:17:23 +0100

    Completed Login Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import ForgetPassword from "./Component/ForgetPassword/ForgetPassword";
+import LogIn from "./Component/LogIn/LogIn";
+import SignUp from "./Component/SignUP/SignUp";
+
+
 
 
 function App() {
   return (
     <div className="App">
-      <h1>projects setup</h1>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<LogIn/>} />
+            <Route path="/SignUp" element={<SignUp/>} />
+            <Route path="/ForgetPassword" element={<ForgetPassword/>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/Component/ForgetPassword/ForgetPassword.js
@@ -0,0 +1,12 @@
+import "./ForgetPassword.css";
+
+
+function ForgetPassword(){
+    return(
+        <div>
+            <h1>Reset Password Page</h1>
+        </div>
+    )
+}
+
+export default ForgetPassword;

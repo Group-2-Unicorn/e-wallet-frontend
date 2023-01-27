786956326a150bad01d4ccf50bc41d36fe3cca35
Isah Jacob <isahjacob0@gmail.com>
2023-01-27 11:01:21 +0100

    OTP component is created and added to the OTP page. The OTP page is also created and added to the router. The OTP page is also added to the Sign Up page.

--- src/Components/OTPInputPage/OTP.js
+++ src/Components/OTPInputPage/OTP.js
@@ -1,13 +1,76 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import "../VerifyPage/verify.css"
+import './otp.css'
 import image from "../Assets/img.jpeg";
 
 
+
+
 const OTP = () => {
+
     return (
-        <div className="Signup-container">
-            <div className="left-side"> 
-                <img className="images" src={image} alt=""/>
+        <div className="otp-container">
+            <div className="otp-left-side"> 
+                <img className="image" src={image} alt=""/>
+            </div>
+
+            <div className="otp-right-container">
+                <div className="otp-form-container">
+                    <div className="otp-right-side">
+                        <p className='otp-text'>Enter your OTP number</p>
+                    </div>
+                    <div className="otp-form-div">
+                        <form className='otp-form'>
+                            <label>
+                                <input 
+                                className="input-btn"
+                                type="password"
+                                placeholder="-"
+                                required
+                                />
+                            </label>
+
+                            <label>
+                                <input 
+                                className="input-btn"
+                                type="password"
+                                placeholder="-"
+                                required
+                                />
+                            </label>
+
+                            <label>
+                                <input 
+                                className="input-btn"
+                                type="password"
+                                placeholder="-"
+                                required
+                                />
+                            </label>
+
+                            <label>
+                                <input 
+                                className="input-btn"
+                                type="password"
+                                placeholder="-"
+                                required
+                                />
+                            </label>
+                        </form>
+                        <button 
+                            className="otp-btn"
+                            to="/Verification" 
+                            // onClick={() => setShow(true)}
+                        >
+                        <Link >
+                            {""}
+                        </Link>
+                        <a href="/Verification" style={{textDecoration: "none"}}>
+                        <span className="otpbtn">Continue</span> 
+                   </a></button>
+                    </div>
+                </div>
             </div>
         </div>
     );

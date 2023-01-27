76e9fa508429404af4b934afd9665cfaa9d5576b
Isah Jacob <isahjacob0@gmail.com>
2023-01-27 14:46:40 +0100

    resolved merge conflicts

--- src/Components/VerifyPage/Verification.js
+++ src/Components/VerifyPage/Verification.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import './verify.css'
-<<<<<<< HEAD
 import image from "../Assets/img.jpeg"
 import SignUp from "../SignUP/SignUp";
 import { Link } from "react-router-dom";
@@ -10,38 +9,11 @@ const Verification = props => {
   if (!props.show){
     return null
   }
-=======
-import SignUp from "../SignUP/SignUp";
-import { Link } from "react-router-dom";
-
-const Verification = props => {
-
-  if (!props.show){
-    return null
-  }
-
-  // const close = (e) => {
-  //   if (e.target.className === 'content-container'){
-  //     props.onClose()
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   document.body.addEventListener('click', close)
-  //   return () => {
-  //     document.body.removeEventListener('click', close)
-  //   }
-  // }, [])
-
->>>>>>> baa92fa7d723defcc842827d1a5d347c5b69b889
   return (
     <div className="verify-container">
       <div className="Signup-container">
           <div className="left-side"> 
-<<<<<<< HEAD
             <img className="images" src={image} alt=""/>
-=======
->>>>>>> baa92fa7d723defcc842827d1a5d347c5b69b889
           </div>
 
         <div className="right-side-container">
@@ -100,46 +72,21 @@ const Verification = props => {
             </div>
             <button 
               className="signup-btn"
-<<<<<<< HEAD
                 to="/Verification" 
             >
             <Link className="login-option" to="/Verification" style={{textDecoration: "none"}}>
                 {""}
             </Link>
               <a href="/Verification" style={{textDecoration: "none"}}>
-=======
-              to="/LogIn" 
-             
-            >
-            <Link 
-              className="login-option" 
-              to="/LogIn" 
-              style={{textDecoration: "none"}}>
-              {""}
-            </Link>
-              <a 
-                href="/LogIn" 
-                style={{textDecoration: "none"}}>
->>>>>>> baa92fa7d723defcc842827d1a5d347c5b69b889
                 <span className="signbtn">Sign Up</span> 
               </a></button>
             </div>
           </div>
         </div>
-<<<<<<< HEAD
       <div className="content-container">
         
         <div className="content-holder">
           <h1 className="content"> Kindly Check your mail to see your OTP number for Account verification</h1>
-=======
-      <div className="content-container" onClick={props.onClose}>
-        
-        <div className="content-holder" onClick={e => e.stopPropagation()}>
-          <h1 
-            className="content"  
-            onClick={props.onClose}
-          > Kindly Check your mail to see your OTP number for Account verification</h1>
->>>>>>> baa92fa7d723defcc842827d1a5d347c5b69b889
         </div>
       </div>
     </div>

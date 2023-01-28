ee8758173848b2991d29fbaab703f9c2e80137d1
Isah Jacob <isahjacob0@gmail.com>
2023-01-28 12:58:19 +0100

    add a function that will take an userDetail as an argument and will send the data using the POST method to the API.

--- src/Components/APIServices/userDetail.js
+++ src/Components/APIServices/userDetail.js
@@ -2,3 +2,14 @@ export function getUserDetails(){
     return fetch('http://localhost:3000/userDetail')
     .then(data => data.json)
 }
+
+export function setItem(elements){
+    return fetch('http://localhost:3000/userDetail', {
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ elements })
+    })
+    .then(data => data.json())
+}

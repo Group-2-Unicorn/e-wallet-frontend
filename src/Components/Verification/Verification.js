05511742d82261a7d0759e6b90a8accfeca5d842
Isah Jacob <isahjacob0@gmail.com>
2023-01-28 10:39:07 +0100

    update

--- src/Components/Verification/Verification.js
+++ src/Components/Verification/Verification.js
@@ -9,19 +9,21 @@ const Verification = props => {
 
   return (
     <div className="verification" onClick={props.onClose}>
+      <Link className="login-option" to="/OTP" style={{textDecoration: "none"}}>
+                {""}
+                onClick={props.onClose}
+      </Link>
 
       
       <div className="verification-content" onClick={e => e.stopPropagation() }>
         <div className="verification-body" onClick={props.onClose}>
 
             <p>
-             Kindly Check your mail to see your OTP number for Account verification
+                Kindly Check your mail to see your OTP number for Account verification
             </p>
         </div>
       </div>
-      <Link className="login-option" to="/OTP" style={{textDecoration: "none"}}>
-        {""}
-      </Link>
+      
     </div>
   );
 }

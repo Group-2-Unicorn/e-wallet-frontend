e7fa5b445452a077d9181a63dd554d8971420559
Isah Jacob <isahjacob0@gmail.com>
2023-01-29 22:05:49 +0100

    added the function that fetch the login api

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,29 +1,68 @@
-
 import React, { useState } from "react";
 import "./LogIn.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import LoginImg from "../Assets/login.jpeg";
+import { useNavigate } from "react-router-dom";
+
+
 
 function LogIn() {
-  const [email, setEmail] = useState([]);
-  const [password, setPassword] = useState([]);
+  const [userLoginDetail, setUserLoginDetail] = useState({
+    password: "",
+    emailAddress: ""
+  });
 
-  const baseUrl = "https://jsonplaceholder.typicode.com/post";
 
-  const postData = (e) => {
-    e.preventDafualt();
-    axios
-      .post(baseUrl, {
-        email,
-        password,
-      })
-      .then((res) => console.log(res))
-      .catch((res) => console.log(res));
 
-    setEmail("");
-    setPassword("");
-  };
+  const handleChange = (event) => {
+    const {name, value} = event.target
+    setUserLoginDetail((prevState) => {
+        return {
+            ...prevState,
+            [name]: value
+        }
+    })
+   }
+
+   const navigate = useNavigate()
+
+  const baseUrl = "https://localhost:3000/api/v1/users/login";
+
+  const validateUser = async (event) => {
+    event.preventDefault()
+       console.log(userLoginDetail)
+       const response = await fetch(baseUrl, {
+           method: 'POST',
+           body: JSON.stringify(userLoginDetail),
+           headers: {
+               "Content-type": "application/json"
+           }
+       })
+       const data = await response.json()
+       console.log(data)
+       navigate("/Dashboard", {
+        state:{
+            emailAddress: userLoginDetail.emailAddress,
+            password: userLoginDetail.password
+        }
+       })
+
+   };
+
+  // const postData = (e) => {
+  //   e.preventDefault();
+  //   axios
+  //     .post(baseUrl, {
+  //       email,
+  //       password,
+  //     })
+  //     .then((res) => console.log(res))
+  //     .catch((res) => console.log(res));
+
+  //   setEmail("");
+  //   setPassword("");
+  // };
 
   return (
     <div className="login">
@@ -40,8 +79,9 @@ function LogIn() {
               placeholder="Email"
               className="input_box"
               type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={handleChange}
+              value={userLoginDetail.emailAddress} 
+              
               required
             />
           </label>
@@ -52,15 +92,15 @@ function LogIn() {
               placeholder="Password"
               className="input_box"
               type="text"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={userLoginDetail.password} 
+              onChange={handleChange}
               required
             />
           </label>
           </div>
           <div className="remember">
             <div className="checker">
-              <input type="checkbox" value={postData.rememberMe}/>
+              <input type="checkbox" value={validateUser.rememberMe}/>
               <span className="rem-me">Remember Me</span>
             </div>
               <p>                
@@ -75,7 +115,7 @@ function LogIn() {
                 </p>
             </div>
           </div>
-          <button className="login_submit" onClick={postData}>
+          <button className="login_submit" onClick={validateUser={}}>
             Log In
           </button>
           <div className="sign_link">

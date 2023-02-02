f1dba06dac9e0d50bac12b76e8d06943f7549738
Isah Jacob <isahjacob0@gmail.com>
2023-02-02 20:28:23 +0100

    modified the Verification js file

--- src/Components/Verification/Verification.js
+++ src/Components/Verification/Verification.js
@@ -1,31 +1,52 @@
-import React from "react";
-import "./verification.css";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import './verification.css'
 
-const Verification = props => {
-    if (!props.show){
-        return null;
-    }
+
+const Verification = () => {
+  const [showModal, setShowModal] = useState(false);
+
+
+  const toggleModal = () => setShowModal(!showModal);
+
 
   return (
-    <div className="verification" onClick={props.onClose}>
-      <Link className="login-option" to="/OTP" style={{textDecoration: "none"}}>
-                {""}
-                onClick={props.onClose}
-      </Link>
-
-      
-      <div className="verification-content" onClick={e => e.stopPropagation() }>
-        <div className="verification-body" onClick={props.onClose}>
-
-            <p>
-                Kindly Check your mail to see your OTP number for Account verification
-            </p>
+    <div
+      className={`modal ${showModal ? "show" : "hide"}`}
+      tabIndex="-1"
+      role="dialog"
+    >
+      <div className="modal-dialog" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Success</h5>
+            <button
+              type="button"
+              className="close"
+              data-dismiss="modal"
+              aria-label="Close"
+              onClick={toggleModal}
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <p>Your action was successful.</p>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-dismiss="modal"
+              onClick={toggleModal}
+            >
+              Close
+            </button>
+          </div>
         </div>
       </div>
-      
     </div>
   );
-}
+};
 
 export default Verification;

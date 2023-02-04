10d34d1b2bd999ddeb40a4bedfe4e2bcf62a7f5c
Isah Jacob <isahjacob0@gmail.com>
2023-02-04 02:09:41 +0100

    added button Component to ForgetPassword

--- src/Components/ForgetPassword/ForgetPassword.js
+++ src/Components/ForgetPassword/ForgetPassword.js
@@ -1,7 +1,8 @@
 import "./ForgetPassword.css"
 import React, { useState , useEffect } from 'react';
 import ResetImg from "../Assets/resetPassword.png"
-
+import flowImage from "../Assets/resetFlow.png"
+import Button from "../ReUsableComponent/Button/Button";
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState('');
@@ -15,21 +16,40 @@ const ForgotPassword = () => {
   return (
         <div className="reset-password-container">
             <div className="reset-password-left-container">
-                <img src={ResetImg} alt=""/>
+                <img className="resetPasswordImage" src={ResetImg} alt=""/>
             </div>
-            <div className="reset_right">
-                <div>
-                    <h4>Enter Email Address to reset Password</h4>
-                <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    value={email}
-                    onChange={(event) => setEmail(event.target.value)}
-                    placeholder="Email address"
-                    required
-                />
-                <button type="submit">NEXT</button>
-                {message && <p>{message}</p>}
+            <div className="reset-password-right-container">
+                <div className="form-reset-container">
+                    <h4 className="reset-password-text">Reset Password </h4>
+                    <img className="reset-flow-image" src={flowImage} alt=""/>
+                    <p className="header-text">Enter Email Address to reset Password</p>
+                    
+                    <form className="forms" onSubmit={handleSubmit}>
+                        <input
+                            className="reset-password-input"
+                            type="email"
+                            value={email}
+                            onChange={(event) => setEmail(event.target.value)}
+                            placeholder="Email address"
+                            required
+                        />
+                        <Button 
+                            name="NEXT"
+                            width="72%"
+                            height="65px"
+                            backgroundColor="#55229e"
+                            border="none"
+                            outline="none"
+                            color="white"
+                            borderRadius="10px"
+                            padding="20px"
+                            fontSize="large"
+                            cursor="pointer"
+                            margin-top="20px"
+                            type="submit"
+                        />
+
+                    {message && <p>{message}</p>}
                 </form>
             </div>
             </div>
